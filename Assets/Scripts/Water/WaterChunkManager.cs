9cd245197f67bcb7f972edecb43d3b08bda943a0
ahernot <70817470+ahernot@users.noreply.github.com>
2021-05-07 21:01:58 +0200

    add script folders, begin implementing water

---
+++ Assets/Scripts/Water/WaterChunkManager.cs
@@ -0,0 +1,195 @@
+﻿/*
+ Copyright Anatole Hernot, 2021
+ All rights reserved
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterChunkManager : MonoBehaviour
+{
+
+    [Tooltip("Player (chunk loader)")]
+    public GameObject player;
+
+    // Player chunk position (updated at runtime)
+    int xChunkPlayer;
+    int zChunkPlayer;
+
+    [Tooltip("Chunk material")]
+    public Material material;
+
+    // List of chunks
+    [HideInInspector]
+    public GameObject[] chunks;
+
+    // [Space(30)]
+
+    [Header("Generation Settings")]
+    // Number of chunks
+    [Tooltip("Half number of chunks on the x-axis")]
+    public int xHalfNbChunks = 64;
+    [Tooltip("Half number of chunks on the z-axis")]
+    public int zHalfNbChunks = 64;
+
+    // Chunks to load (square of side 2x+1)
+    [Tooltip("Number of chunks to load in each direction around the player")]
+    public int loadHighRadius = 8;
+
+    // Wave parameters
+    public Vector2 waveDirection;
+    public float waveAmplitude;
+    public float waveVariation;
+    public float waveSpeed;
+
+
+
+    //----------------- TO CHANGE
+    [Header("Chunk Settings")]
+    // Size of a chunk (affects texture size)
+    [Tooltip("x-axis size of the chunk (in world units)")]
+    public int xChunkSize = 16;
+    [Tooltip("z-axis size of the chunk (in world units)")]
+    public int zChunkSize = 16;
+
+    // Chunk mesh poly count (fineness)
+    [Tooltip("Number of polygons per chunk along the x-axis")]
+    public int xNbPolygons = 32;
+    [Tooltip("Number of polygons per chunk along the z-axis")]
+    public int zNbPolygons = 32;
+
+    [Header("Mesh Resolution")]
+    [Tooltip("Mesh reduction ratio along the x-axis")]
+    [Range(1, 128)]
+    public int xReductionRatio = 4;
+    [Tooltip("Mesh reduction ratio along the z-axis")]
+    [Range(1, 128)]
+    public int zReductionRatio = 4;
+
+    //----------------- TO CHANGE
+
+
+
+
+
+
+    void Start ()
+    {
+        this.DestroyChunks();
+        this.GenerateChunks();
+    }
+
+    /**
+    * Regenerate the chunks (with their noise maps)
+    **/
+    public void GenerateChunks ()
+    {
+
+        // Initialise chunks array
+        this.chunks = new GameObject[this.xHalfNbChunks * this.zHalfNbChunks * 4];
+
+        int i = 0;
+        for (int xChunkId = -1 * this.xHalfNbChunks; xChunkId < this.xHalfNbChunks; xChunkId++)
+        {
+            for (int zChunkId = -1 * this.zHalfNbChunks; zChunkId < this.zHalfNbChunks; zChunkId++)
+            {
+                // Initialise empty GameObject
+                this.chunks[i] = new GameObject();
+                this.chunks[i] .name = "Chunk_" + xChunkId.ToString() + "_" + zChunkId.ToString();
+                this.chunks[i] .transform.parent = gameObject.transform; // set parent
+                this.chunks[i] .layer = 8;
+
+                // Update position and rotation
+                this.chunks[i] .transform.position = new Vector3(xChunkId * this.xChunkSize, 0, zChunkId * this.zChunkSize);
+                this.chunks[i] .transform.rotation = Quaternion.identity;
+
+                // Add necessary components
+                this.chunks[i] .AddComponent<MeshFilter>();
+                this.chunks[i] .AddComponent<MeshCollider>();
+                this.chunks[i] .AddComponent<MeshRenderer>();
+
+                MeshRenderer meshRenderer = this.chunks[i] .GetComponent<MeshRenderer>();
+                meshRenderer.material = (Material)Instantiate(this.material);
+
+                // Create ChunkMesh component
+                ChunkMesh chunkMesh = this.chunks[i] .AddComponent<ChunkMesh>();
+
+                // Set ChunkMesh parameters
+                chunkMesh.xChunk = xChunkId;
+                chunkMesh.zChunk = zChunkId;
+                chunkMesh.xChunkSize = this.xChunkSize;
+                chunkMesh.zChunkSize = this.zChunkSize;
+                chunkMesh.xNbPolygons = this.xNbPolygons;
+                chunkMesh.zNbPolygons = this.zNbPolygons;
+                chunkMesh.xReductionRatio = this.xReductionRatio;
+                chunkMesh.zReductionRatio = this.zReductionRatio;
+
+                // Set noise parameters
+                chunkMesh.noiseLayers = this.noiseLayers;
+
+                // chunkMesh.noiseScale = this.noiseScale;
+                // chunkMesh.noiseOctaves = this.noiseOctaves;
+                // chunkMesh.noiseAmplitudeMult = this.noiseAmplitudeMult;
+                // chunkMesh.noiseFrequencyMult = this.noiseFrequencyMult;
+                // chunkMesh.noiseMultiplier = this.noiseMultiplier;
+
+                i++;
+            }
+        }
+    }
+
+    public void DestroyChunks ()
+    {
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    void Update ()
+    {
+        this.GetPlayerChunk();
+        this.UpdateResolutions();
+    }
+
+    void GetPlayerChunk ()
+    {
+        float x = this.player .transform.position.x;
+        float z = this.player .transform.position.z;
+
+        this.xChunkPlayer = (int) Mathf.Floor((float)x / this.xChunkSize);
+        this.zChunkPlayer = (int) Mathf.Floor((float)z / this.zChunkSize);
+    }
+
+    void UpdateResolutions ()
+    {
+        int i = 0;
+        for (int xChunkId = -1 * this.xHalfNbChunks; xChunkId < this.xHalfNbChunks; xChunkId++)
+        {
+            for (int zChunkId = -1 * this.zHalfNbChunks; zChunkId < this.zHalfNbChunks; zChunkId++)
+            {
+                // Continue if chunk out of bounds (nb of chunks was increased but not yet generated)
+                if (i >= this.chunks.Length) { continue; }
+
+                // Get chunk's mesh script
+                ChunkMesh chunkMesh = this.chunks[i] .GetComponent<ChunkMesh>();
+
+                if ((zChunkId >= this.zChunkPlayer - this.loadHighRadius) && (zChunkId <= this.zChunkPlayer + this.loadHighRadius))
+                {
+                    if ((xChunkId >= this.xChunkPlayer - this.loadHighRadius) && (xChunkId <= this.xChunkPlayer + this.loadHighRadius))
+                    {
+                        chunkMesh.meshResolution = ChunkMesh.MeshResolution.High;
+                    } else {
+                        chunkMesh.meshResolution = ChunkMesh.MeshResolution.Medium;
+                    };
+                } else {
+                    chunkMesh.meshResolution = ChunkMesh.MeshResolution.Medium;
+                };
+
+                i ++;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Water/WaterChunkMesh.cs
@@ -0,0 +1,413 @@
+﻿/*
+ Copyright Anatole Hernot, 2021
+ All rights reserved
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+// [RequireComponent(typeof(MeshCollider))]
+public class WaterChunkMesh : MonoBehaviour
+{
+
+    // Chunk coordinates
+    [HideInInspector]
+    public int xChunk = 0;
+    [HideInInspector]
+    public int zChunk = 0;
+
+
+    [Header("Generation Settings")]
+    // Chunk dimensions (determined by ChunkManager)
+    public int xChunkSize = 16;
+    public int zChunkSize = 16;
+
+    // Chunk mesh dimensions
+    public int xNbPolygons = 32;
+    public int zNbPolygons = 32;
+    float[] xVerticesRel;
+    float[] zVerticesRel;
+    
+    [Header("Mesh Resolution")]
+    [Range(1, 128)]
+    public int xReductionRatio = 4;
+    [Range(1, 128)]
+    public int zReductionRatio = 4;
+
+    public enum MeshResolution {Low, Medium, High};
+    public MeshResolution meshResolution;
+    
+    // Initialise mesh resolutions
+    Mesh meshLow;
+    Mesh meshMed;
+    Mesh meshHigh;
+
+    // Low = default (flat)
+    Vector3[] verticesLow;
+    int[] trianglesLow;
+    Vector2[] uvsLow;
+
+    // Med = low poly relief
+    Vector3[] verticesMed;
+    int[] trianglesMed;
+    Vector2[] uvsMed;
+
+    // High = high poly
+    Vector3[] verticesHigh;
+    int[] trianglesHigh;
+    Vector2[] uvsHigh;
+
+    // Wave parameters
+    public Vector2 waveDirection;
+    public float waveAmplitude;
+    public float waveVariation;
+    public float waveSpeed;
+
+    /**
+    * Clamp parameters to prevent errors
+    **/
+    void ClampParameters ()
+    {
+        if (this.xChunkSize <= 0)
+        {
+            this.xChunkSize = 1;
+        }
+        if (this.zChunkSize <= 0)
+        {
+            this.zChunkSize = 1;
+        }
+
+        if (this.xNbPolygons <= 0)
+        {
+            this.xNbPolygons = 1;
+        }
+        if (this.zNbPolygons <= 0)
+        {
+            this.zNbPolygons = 1;
+        }
+    }
+
+    /**
+    * Initialise chunk
+    * Start is called before the first frame update
+    **/
+    void Start ()
+    {
+        // Check parameters
+        this.ClampParameters();
+
+        // Initialise vertices' relative positions
+        this.xVerticesRel = new Functions().LinearRange (0, this.xChunkSize, this.xNbPolygons + 1);
+        this.zVerticesRel = new Functions().LinearRange (0, this.zChunkSize, this.zNbPolygons + 1);
+
+        // Generate all meshes
+        this.GenerateMeshLow();
+        this.GenerateMeshMed();
+        this.GenerateMeshHigh();
+    }
+
+    /**
+    * Update chunk
+    **/
+    void Update ()
+    {
+        // Set chosen mesh
+        this.SetMesh();
+    }
+
+    /**
+    * Destroy chunk
+    **/
+    ~ChunkMesh ()
+    {
+        this.meshLow.Clear();
+        this.meshMed.Clear();
+        this.meshHigh.Clear();
+
+        Array.Clear(this.verticesLow, 0, this.verticesLow.Length);
+        Array.Clear(this.trianglesLow, 0, this.trianglesLow.Length);
+        Array.Clear(this.uvsLow, 0, this.uvsLow.Length);
+
+        Array.Clear(this.verticesMed, 0, this.verticesMed.Length);
+        Array.Clear(this.trianglesMed, 0, this.trianglesMed.Length);
+        Array.Clear(this.uvsMed, 0, this.uvsMed.Length);
+
+        Array.Clear(this.verticesHigh, 0, this.verticesHigh.Length);
+        Array.Clear(this.trianglesHigh, 0, this.trianglesHigh.Length);
+        Array.Clear(this.uvsHigh, 0, this.uvsHigh.Length);
+
+        Array.Clear(this.noiseMap, 0, this.noiseMap.Length);
+    }
+
+    /**
+    * Set mesh (low / med / high)
+    **/
+    void SetMesh ()
+    {
+        if (this.meshResolution == MeshResolution.Low) {
+            GetComponent<MeshFilter>().mesh = this.meshLow;
+            // GetComponent<MeshCollider>().sharedMesh = this.meshLow;
+        } else if (this.meshResolution == MeshResolution.Medium) {
+            GetComponent<MeshFilter>().mesh = this.meshMed;
+            // GetComponent<MeshCollider>().sharedMesh = this.meshMed;
+        } else if (this.meshResolution == MeshResolution.High) {
+            GetComponent<MeshFilter>().mesh = this.meshHigh;
+            // GetComponent<MeshCollider>().sharedMesh = this.meshHigh;
+        }
+
+    }
+
+    /**
+    * Generate low resolution mesh (flat)
+    * Run on instance initialisation
+    **/
+    private void GenerateMeshLow ()
+    {
+        // Initialise mesh
+        this.meshLow = new Mesh();
+
+        // Generate vertices and uvs
+        this.verticesLow = new Vector3 [4];
+        this.uvsLow = new Vector2 [4];
+        int i = 0;
+        for (int zVertexRel = 0; zVertexRel <= this.zChunkSize; zVertexRel += this.zChunkSize) // increment to only select bounds
+        {
+            for (int xVertexRel = 0; xVertexRel <= this.xChunkSize; xVertexRel += this.xChunkSize) // increment to only select bounds
+            {
+                // Add vertex
+                this.verticesLow [i] = new Vector3 (xVertexRel, 0, zVertexRel);
+
+                // Add uv
+                this.uvsLow [i] = new Vector2 (
+                    (float) xVertexRel / this.xChunkSize,
+                    (float) zVertexRel / this.zChunkSize
+                );
+
+                i ++;
+            }
+        }
+
+        // Generate triangles
+        this.trianglesLow = new int [6] {
+            0, 3, 1,
+            0, 2, 3
+        };
+
+        // Fill mesh
+        this.meshLow .Clear();
+        this.meshLow.vertices = this.verticesLow;
+        this.meshLow.triangles = this.trianglesLow;
+        this.meshLow.uv = this.uvsLow;
+        this.meshLow .RecalculateNormals();
+    }
+
+    /**
+    * Generate medium resolution mesh (low detail)
+    * Run on instance initialisation
+    **/
+    private void GenerateMeshMed ()
+    {
+        // Initialise mesh
+        this.meshMed = new Mesh();
+
+        // Calculate number of polygons per side
+        int xNbPolygonsMed = (int) Mathf.Ceil (this.xNbPolygons / this.xReductionRatio); // max between this and 1
+        if (xNbPolygonsMed <= 0) { xNbPolygonsMed = 1; }
+        int zNbPolygonsMed = (int) Mathf.Ceil (this.zNbPolygons / this.zReductionRatio); // max between this and 1
+        if (zNbPolygonsMed <= 0) { zNbPolygonsMed = 1; }
+
+        // Calculate index step
+        int xIdStep = (int) Mathf.Floor (this.xNbPolygons / xNbPolygonsMed); // floor to avoid overrun
+        int zIdStep = (int) Mathf.Floor (this.zNbPolygons / zNbPolygonsMed); // floor to avoid overrun
+
+        // Initialise vertices Vector3 array
+        this.verticesMed = new Vector3 [(xNbPolygonsMed + 1) * (zNbPolygonsMed + 1)];
+        // Initialise uvs Vector2 array
+        this.uvsMed = new Vector2 [this.verticesMed .Length];
+
+        // Initialise relative vertex positions
+        float xVertexRel;
+        float yVertexRel;
+        float zVertexRel;
+
+        int i = 0;
+        for (int zVertexId = 0; zVertexId < zNbPolygonsMed; zVertexId ++)
+        {
+            // Get z vertex coordinate
+            zVertexRel = this.zVerticesRel [zVertexId * zIdStep];
+
+            for (int xVertexId = 0; xVertexId < xNbPolygonsMed; xVertexId ++)
+            {
+                // Get x vertex coordinate
+                xVertexRel = this.xVerticesRel [xVertexId * xIdStep];
+
+                // Get y vertex coordinate using the noise map
+                yVertexRel = 0f;
+
+                // Add vertex
+                this.verticesMed [i] = new Vector3 (xVertexRel, yVertexRel, zVertexRel);
+
+                // Add uv
+                this.uvsMed [i] = new Vector2 (
+                    (float) xVertexRel / this.xChunkSize,
+                    (float) zVertexRel / this.zChunkSize
+                );
+
+                i ++;
+            }
+
+            // Add end vertices and uvs for x=X_MAX
+            xVertexRel = this.xVerticesRel [this.xNbPolygons];
+            yVertexRel = = 0f;
+
+            this.verticesMed [i] = new Vector3 (xVertexRel, yVertexRel, zVertexRel);
+
+            this.uvsMed [i] = new Vector2 (
+                (float) xVertexRel / this.xChunkSize,
+                (float) zVertexRel / this.zChunkSize
+            );
+
+            i ++;
+
+        }
+
+        // Add end vertices and uvs for z=Z_MAX
+        zVertexRel = this.zVerticesRel [this.zNbPolygons];
+        for (int xVertexId = 0; xVertexId < xNbPolygonsMed; xVertexId ++)
+        {
+            xVertexRel = this.xVerticesRel [xVertexId * xIdStep];
+            yVertexRel = 0f;
+
+            this.verticesMed [i] = new Vector3 (xVertexRel, yVertexRel, zVertexRel);
+
+            this.uvsMed [i] = new Vector2 (
+                (float) xVertexRel / this.xChunkSize,
+                (float) zVertexRel / this.zChunkSize
+            );
+
+            i ++;
+        }
+
+        // Add final vertex
+        xVertexRel = this.xVerticesRel [this.xNbPolygons];
+        zVertexRel = this.zVerticesRel [this.zNbPolygons];
+        yVertexRel = 0f;
+
+        this.verticesMed [i] = new Vector3 (xVertexRel, yVertexRel, zVertexRel);
+
+        this.uvsMed [i] = new Vector2 (
+            (float) xVertexRel / this.xChunkSize,
+            (float) zVertexRel / this.zChunkSize
+        );
+
+        // Generate triangles
+        this.trianglesMed = new int [xNbPolygonsMed * zNbPolygonsMed * 6];
+        int vert = 0;
+        int tris = 0;
+        for (int zVertexId = 0; zVertexId < zNbPolygonsMed; zVertexId ++)
+        {
+            for (int xVertexId = 0; xVertexId < xNbPolygonsMed; xVertexId ++)
+            {
+                this.trianglesMed [tris + 0] = vert + 0;
+                this.trianglesMed [tris + 1] = vert + xNbPolygonsMed + 1;
+                this.trianglesMed [tris + 2] = vert + 1;
+                this.trianglesMed [tris + 3] = vert + 1;
+                this.trianglesMed [tris + 4] = vert + xNbPolygonsMed + 1;
+                this.trianglesMed [tris + 5] = vert + xNbPolygonsMed + 2;
+
+                vert ++;
+                tris += 6;
+            }
+            vert ++;
+        }
+
+        // Fill mesh
+        this.meshMed .Clear();
+        this.meshMed.vertices = this.verticesMed;
+        this.meshMed.triangles = this.trianglesMed;
+        this.meshMed.uv = this.uvsMed;
+        this.meshMed .RecalculateNormals();
+    }
+
+    /**
+    * Generate high resolution mesh (high detail)
+    * Run on instance initialisation
+    **/
+    private void GenerateMeshHigh ()
+    {
+
+        // Initialise mesh
+        this.meshHigh = new Mesh();
+
+        // Initialise vertices Vector3 array
+        this.verticesHigh = new Vector3 [(this.xNbPolygons + 1) * (this.zNbPolygons + 1)];
+        // Initialise uvs Vector2 array
+        this.uvsHigh = new Vector2 [this.verticesHigh .Length];
+
+        // Initialise relative vertex positions
+        float xVertexRel;
+        float yVertexRel;
+        float zVertexRel;
+
+        int i = 0;
+        for (int zVertexId = 0; zVertexId <= this.zNbPolygons; zVertexId ++)
+        {
+            // Get z vertex coordinate
+            zVertexRel = this.zVerticesRel [zVertexId];
+
+            for (int xVertexId = 0; xVertexId <= this.xNbPolygons; xVertexId ++)
+            {
+                // Get x vertex coordinate
+                xVertexRel = this.xVerticesRel [xVertexId];
+
+                // Get y vertex coordinate using the noise map
+                yVertexRel = 0f;
+
+                // Add vertex
+                this.verticesHigh [i] = new Vector3 (xVertexRel, yVertexRel, zVertexRel);
+
+                // Add uv
+                this.uvsHigh [i] = new Vector2 (
+                    (float) xVertexRel / this.xChunkSize,
+                    (float) zVertexRel / this.zChunkSize
+                );
+
+                i ++;
+            }
+        }
+
+        // Initialise triangles int array
+        this.trianglesHigh = new int [this.xNbPolygons * this.zNbPolygons * 6];
+
+        int vert = 0;
+        int tris = 0;
+        for (int zVertexId = 0; zVertexId < this.zNbPolygons; zVertexId ++)
+        {
+            for (int xVertexId = 0; xVertexId < this.xNbPolygons; xVertexId ++)
+            {
+                this.trianglesHigh [tris + 0] = vert + 0;
+                this.trianglesHigh [tris + 1] = vert + this.xNbPolygons + 1;
+                this.trianglesHigh [tris + 2] = vert + 1;
+                this.trianglesHigh [tris + 3] = vert + 1;
+                this.trianglesHigh [tris + 4] = vert + this.xNbPolygons + 1;
+                this.trianglesHigh [tris + 5] = vert + this.xNbPolygons + 2;
+
+                vert ++;
+                tris += 6;
+            }
+            vert ++;
+        }
+
+        // Fill mesh
+        this.meshHigh .Clear();
+        this.meshHigh.vertices = this.verticesHigh;
+        this.meshHigh.triangles = this.trianglesHigh;
+        this.meshHigh.uv = this.uvsHigh;
+        this.meshHigh .RecalculateNormals();
+    }
+
+
+}
