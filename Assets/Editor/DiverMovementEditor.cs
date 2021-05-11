1fe0e3a72b447d58538a42f57a9932b8728fbc2f
ahernot <70817470+ahernot@users.noreply.github.com>
2021-05-11 21:03:29 +0200

    finish implementing DiverMovement.cs

---
+++ Assets/Editor/DiverMovementEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (DiverMovement))]
+public class DiverMovementEditor : Editor
+{
+    // Override default inspector
+    public override void OnInspectorGUI()
+    {
+        // Fetch target
+        DiverMovement diverMovement = (DiverMovement)target;
+
+        // Draw default inspector
+        if (DrawDefaultInspector())
+        {
+            DrawDefaultInspector();
+        }
+
+        EditorGUILayout.Space();
+
+        // Draw RegenerateForces button
+        if (GUILayout.Button ("Regenerate Forces"))
+        {
+            diverMovement.RegenerateForces();
+        }
+
+        // Draw ResetMovement button
+        if (GUILayout.Button ("Reset Movement"))
+        {
+            diverMovement.ResetMovement();
+        }
+
+    }
+}
--- Assets/Scripts/Player/DiverMovement.cs
+++ Assets/Scripts/Player/DiverMovement.cs
@@ -7,21 +7,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DiverMovementNew : MonoBehaviour
+public class DiverMovement : MonoBehaviour
 {
-    // Character controller
+    [Header("Player")]
     public CharacterController controller;
+    [Tooltip("Main FPV camera")]
+    public Camera viewCamera;
 
     [Header("Movement Parameters")]
+    [Tooltip("Player mass (higher = higher inertia)")]
     public float playerMass = 7f;
+    [Tooltip("Water drag multiplier (higher = more drag)")]
     public float dragMultiplier = 4f;
+    [Tooltip("Movement force multiplier (higher = more movement power)")]
     public float movementMultiplier = 20f;
 
-    [Tooltip("Vertical force multiplier (gravity + buoyancy)")]
-    public float verticalForceMultiplier = -3f;
+    [Tooltip("Vertical force multiplier (gravity + buoyancy) — requires force generation on update")]
+    public float verticalForceMultiplier = -0.8f;
 
     [Header("Water Parameters")]
+    [Tooltip("Water current direction vector (not normalized) — requires force generation on update")]
     public Vector3 waterCurrentDirection = new Vector3 (1, 0, 0);
+    [Tooltip("Water current velocity — requires force generation on update")]
     public float waterCurrentVelocity = 0.15f;
 
     [Header("Instantaneous Movement")]
@@ -33,27 +40,21 @@ public class DiverMovementNew : MonoBehaviour
     Vector3 verticalForce;
     Vector3 waterVelocity;
 
-
     void Start ()
     {
         // Initialise player velocity
         this.velocity = new Vector3 ();
 
-        // Compute vertical force vector
-        this.verticalForce = new Vector3 (0f, this.verticalForceMultiplier, 0f);
-
-        // Compute water velocity vector
-        this.waterVelocity = this.waterCurrentVelocity * Vector3.Normalize (this.waterCurrentDirection);
+        // Generate forces
+        this.RegenerateForces();
     }
 
 
-    void Update()
+    void Update ()
     {
-
-        // Get camera direction
-        // TODO: use child camera instead
-        Vector3 cameraForward = Camera.main.transform.forward;
-        Vector3 cameraRight = Camera.main.transform.right;
+        // Get camera direction vectors
+        Vector3 cameraForward = this.viewCamera .transform.forward;  // or Camera.main
+        Vector3 cameraRight = this.viewCamera .transform.right;  // or Camera.main
 
         // Get player input
         float inputLateral = Input.GetAxis("Horizontal");  // lateral axis (left / right)
@@ -69,5 +70,21 @@ public class DiverMovementNew : MonoBehaviour
 
         // Move player
         controller.Move (this.velocity * Time.deltaTime);
+    }
+
+
+    public void RegenerateForces ()
+    {
+        // Compute vertical force vector
+        this.verticalForce = new Vector3 (0f, this.verticalForceMultiplier, 0f);
+
+        // Compute water velocity vector
+        this.waterVelocity = this.waterCurrentVelocity * Vector3.Normalize (this.waterCurrentDirection);
+    }
+
+
+    public void ResetMovement ()
+    {
+        this.velocity = Vector3.zero;
     }
 }
