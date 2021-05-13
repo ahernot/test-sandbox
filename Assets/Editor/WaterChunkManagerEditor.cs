4e85a84bef3ce26861922d90a30e134fbdb0a873
ahernot <70817470+ahernot@users.noreply.github.com>
2021-05-13 20:27:53 +0200

    create WaterChunkManager editor

---
+++ Assets/Editor/WaterChunkManagerEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (WaterChunkManager))]
+public class WaterChunkManagerEditor : Editor
+{
+    // Override default inspector
+    public override void OnInspectorGUI()
+    {
+        // Fetch target
+        WaterChunkManager waterChunkManager = (WaterChunkManager)target;
+
+        // Draw default inspector
+        if (DrawDefaultInspector())
+        {
+            DrawDefaultInspector();
+        }
+
+        EditorGUILayout.Space();
+
+        // Draw Generate button
+        if (GUILayout.Button ("Generate Chunks"))
+        {
+            waterChunkManager.DestroyChunks();
+            waterChunkManager.GenerateChunks();
+        }
+
+    }
+}
