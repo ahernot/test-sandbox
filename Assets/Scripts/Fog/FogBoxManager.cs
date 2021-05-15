5c9fa835110bc5d612c047cbf3b27b7c9b6477d8
ahernot <70817470+ahernot@users.noreply.github.com>
2021-05-15 14:46:06 +0200

    Create FogBoxManager.cs

---
+++ Assets/Scripts/Fog/FogBoxManager.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Copyright Anatole Hernot, 2021
+ All rights reserved
+
+ FogBoxManager v1.0
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogBoxManager : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public int resolution = 64; // 64 vertices per turn
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
