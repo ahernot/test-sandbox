04fd08df33027e2b8e37cf20074c8faf4f5f08fc
ahernot <70817470+ahernot@users.noreply.github.com>
2021-05-15 14:46:18 +0200

    Create FogBoxMeshEditor.cs

---
+++ Assets/Editor/FogBoxMeshEditor.cs
@@ -0,0 +1,43 @@
+﻿/*
+ Copyright Anatole Hernot, 2021
+ All rights reserved
+
+ FogBoxMeshEditor v1.0
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FogBoxMesh))]
+public class FogBoxMeshEditor : Editor
+{
+    // Override default inspector
+    public override void OnInspectorGUI()
+    {
+        // Fetch target
+        FogBoxMesh fogBoxMesh = (FogBoxMesh)target;
+
+        // Draw default inspector
+        if (DrawDefaultInspector())
+        {
+            DrawDefaultInspector();
+        }
+
+        EditorGUILayout.Space();
+
+        // Draw RegenerateForces button
+        if (GUILayout.Button ("Regenerate Forces"))
+        {
+            diverMovement.RegenerateForces();
+        }
+
+        // Draw ResetMovement button
+        if (GUILayout.Button ("Reset Movement"))
+        {
+            diverMovement.ResetMovement();
+        }
+
+    }
+}
