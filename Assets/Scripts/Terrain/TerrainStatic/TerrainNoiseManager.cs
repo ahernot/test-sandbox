8d95ef0e4aefaefd7f52602a9ca247f5d1c2ccd4
ahernot <70817470+ahernot@users.noreply.github.com>
2021-05-19 19:20:35 +0200

    revamp TerrainNoiseManager

--- Assets/Scripts/Terrain/TerrainStatic/TerrainNoiseManager.cs
+++ Assets/Scripts/Terrain/TerrainStatic/TerrainNoiseManager.cs
@@ -2,7 +2,7 @@
  Copyright Anatole Hernot, 2021
  All rights reserved
 
- TerrainNoiseManager v1.1
+ TerrainNoiseManager v2.0
 */
 
 using System.Collections;
@@ -11,133 +11,109 @@ using UnityEngine;
 
 public class TerrainNoiseManager
 {
+    NoiseLayer[] noiseLayers;
 
-    // Noise parameters
-    public NoiseType[] noiseLayers;
+    public TerrainNoiseManagerNew (NoiseLayer[] noiseLayers) {
+        this.noiseLayers = noiseLayers;
+    }
 
-    public float[,] GenerateNoiseMap (Vector2 vertexStart, Vector2 vertexStop, int xNbVertices, int yNbVertices)
+    /**
+    * Generate a noise chunk at coordinates between offset, offset+size
+    */
+    public float[,] GenerateNoiseChunk (Vector2 offset, Vector2 size, int vertsPerSide)
     {
+        // Initialise noiseMap
+        float[,] noiseMap = new float [vertsPerSide, vertsPerSide];
         
-        // Starting values
+        // Initialise amplitude and frequency
         float amplitudeStart = 1f;
         float frequencyStart = 1f;
-
-        // Initialise noiseMap
-        float[,] noiseMap = new float [xNbVertices, yNbVertices];
-
-        // Generate vertex coordinates
-        float[] xVertices = new Functions().LinearRange (vertexStart.x, vertexStop.x, xNbVertices);
-        float[] yVertices = new Functions().LinearRange (vertexStart.y, vertexStop.y, yNbVertices);
-
-        // Loop through map layers
-        // for (int layerId = 0; layerId < noiseLayers.Length; layerId ++)
-        // {
-
-            // Retrieve noise layer
-            NoiseType noiseLayer = this.noiseLayers [0];// [layerId];
 
         // Calculate normalising factor
         float a = amplitudeStart;
         float heightRangeHalf = 0f;
-        for (int octaveId = 0; octaveId < noiseLayer.noiseOctaves; octaveId ++)
+        for (int octaveId = 0; octaveId < octaves; octaveId ++)
         {
             heightRangeHalf += a; // 1 * a
-            a *= noiseLayer.noiseAmplitudeMult; // increment (decrement) persistence after each octave
+            a *= amplitudeMult; // increment (decrement) persistence after each octave
         }
 
-        
-        
-            // Skip layer
-            // if (noiseLayer.renderLayer == false)
-            // {
-            //     continue;
-            // }
+        // Generate vertex coordinates
+        float[] xVerticesRel = Functions.LinearRange (0, size.x, vertsPerSide);
+        float[] yVerticesRel = Functions.LinearRange (0, size.y, vertsPerSide);
 
-            float amplitude;
-            float frequency;
+        // Initialise noise parameters
+        float amplitude;
+        float frequency;
+        float noiseHeight;
 
-            float xVertex;
-            float yVertex;
+        // Initialise coordinates
+        float xVertexRel;
+        float yVertexRel;
 
-            // Initialise sampling coordinates
-            float xSampling;
-            float ySampling;
-
-            // Loop through map pixels
-            for (int yVertexId = 0; yVertexId < yNbVertices; yVertexId ++)
+        // Loop through map pixels
+        for (int yVertexId = 0; yVertexId < vertsPerSide; yVertexId ++)
+        {
+            // Get y vertex coordinate
+            yVertexRel = offset.y + yVerticesRel[yVertexId];
+            
+            for (int xVertexId = 0; xVertexId < vertsPerSide; xVertexId ++)
             {
-                // Get y vertex coordinate
-                yVertex = yVertices [yVertexId];
-                
-                for (int xVertexId = 0; xVertexId < xNbVertices; xVertexId ++)
+                // Get x vertex coordinate
+                xVertexRel = offset.x + xVerticesRel[xVertexId];
+
+                // Initialise noiseHeight
+                noiseHeight = 0f;
+
+                for (int noiseLayerId = 0; noiseLayerId < this.noiseLayers.Length; noiseLayerId ++)
                 {
-                    // Get x vertex coordinate
-                    xVertex = xVertices [xVertexId];
-
-                    float noiseHeight;
-                    float noiseValue;
-
                     amplitude = amplitudeStart;
                     frequency = frequencyStart;
-                    noiseHeight = 0f;
 
-                    for (int octaveId = 0; octaveId < noiseLayer.noiseOctaves; octaveId ++)
+                    // Initialise sampling coordinates
+                    float xSampling;
+                    float ySampling;
+                    float noiseValue;
+
+                    for (int octaveId = 0; octaveId < octaves; octaveId ++)
                     {
-                        xSampling = xVertex / noiseLayer.noiseScale * frequency;
-                        ySampling = yVertex / noiseLayer.noiseScale * frequency;
+                        xSampling = xVertexRel / scale * frequency;
+                        ySampling = yVertexRel / scale * frequency;
+                        xSampling += this.xPerlinOffset;
+                        ySampling += this.yPerlinOffset;
 
                         noiseValue = Mathf.PerlinNoise (xSampling, ySampling) * 2 - 1; // cast to range [-1, 1]
 
                         noiseHeight += noiseValue * amplitude;
 
-                        amplitude *= noiseLayer.noiseAmplitudeMult; // increment (decrement) persistence after each octave
-                        frequency *= noiseLayer.noiseFrequencyMult; // increment frequency
+                        amplitude *= amplitudeMult; // increment (decrement) persistence after each octave
+                        frequency *= frequencyMult; // increment frequency
                     }
-                    
-                    // Apply to noiseMap
-                    noiseMap [xVertexId, yVertexId] += noiseHeight / (2 * heightRangeHalf) * noiseLayer.noiseMultiplier;
-
                 }
+                
+                // Apply to noiseMap
+                noiseMap [xVertexId, yVertexId] = noiseHeight / (2 * heightRangeHalf) * noiseMultiplier;
             }
-        //}
+        }
 
-    return noiseMap;
+        return noiseMap;
 
     }
-
-
-    // private float[] LinearRange (float start, float stop, int nbPoints)
-    // {
-    //     float[] range = new float [nbPoints];
-
-    //     float step = (stop - start) / (float)(nbPoints - 1);
-    //     float point;
-
-    //     // Fill the first nbPoints-1 points of the range
-    //     for (int i = 0; i < nbPoints - 1; i ++)
-    //     {
-    //         point = i * step;
-    //         range [i] = point;
-    //     }
-
-    //     // Make sure that the last point is (float)stop
-    //     range [nbPoints - 1] = (float)stop;
-
-    //     return range;
-    // }
-
-
 }
 
+
+
 [System.Serializable]
-public struct NoiseType {
+public struct NoiseLayer {
     public string name;
     public bool renderLayer;
+    public int seed;
 
-    public float noiseScale; // = 3f;
-    public int noiseOctaves; // = 4;
-    public float noiseAmplitudeMult; // = 2f;
-    public float noiseFrequencyMult; // = 10f;
+    public float scale; // = 3f;
+    public int octaves; // = 4;
+    public float amplitudeMult; // = 2f;
+    public float frequencyMult; // = 10f;
 
-    public float noiseMultiplier; // = 1f;
+    //useless
+    public float multiplier; // = 1f;
 }
